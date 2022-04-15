d97fbf0516fda73700abb78355e47f71ff00c91a
Auroiah Morgan <amwebdev@outlook.com>
2022-04-14 21:55:07 -0400

    init commit
    rebuild app with npx cra to update to latest cra app

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './styles/App.css';
-import { Pokemon, PokedexHome, NotFound404, PokeBattle } from './pages/index';
-import ErrorBoundary from './components/ErrorBoundary';
+import logo from './logo.svg';
+import './App.css';
 
-function App({ login }) {
+function App() {
   return (
-    <Router>
-      <header className='App-header App'>
-        {/* TODO: Change from home to a custom welcome message */}
-        <h1 className='App-title'>Menu</h1>
-        <nav>
-          <Link className='App-btn-link-2' to='/'>
-            Pokedex
-          </Link>
-          <Link className='App-btn-link-2' to='/battle'>
-            Battle
-          </Link>
-        </nav>
-      </header>
-
-      <Switch>
-        <Route exact path='/'>
-          <ErrorBoundary>
-            <PokedexHome />
-          </ErrorBoundary>
-        </Route>
-        <Route path='/pokemon/:id'>
-          <ErrorBoundary>
-            <Pokemon />
-          </ErrorBoundary>
-        </Route>
-        <Route path='/battle'>
-          <ErrorBoundary>
-            <PokeBattle />
-          </ErrorBoundary>
-        </Route>
-        <Route path='*'>
-          <ErrorBoundary>
-            <NotFound404 />
-          </ErrorBoundary>
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+     <h1>Pokedex</h1>
+    </div>
   );
 }
 

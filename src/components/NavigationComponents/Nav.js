84cdc0c37608f6e2e6c03a403281eb6f041679a4
Auroiah Morgan <amwebdev@outlook.com>
2022-04-14 22:58:08 -0400

    Update NavigationComponents

--- src/components/NavigationComponents/Nav.js
+++ src/components/NavigationComponents/Nav.js
@@ -1,7 +1,18 @@
+
+
+
+/**
+ * Returns a nav HTML Element with a nested \<a\> element.
+ * @param  {any} props
+ * @param {string} props.className - CSS classname
+ * @param {object} props.style - Object representing inline-CSS class properties to provide \<nav\> tag.
+ * @param {string} props.route - href route for \<a\> tag
+ * @param {string} props.linkName - name of the link. ex. Home
+ */
 export default function Nav(props) {
   return (
-    <nav style={props.style} className={props.className} href={props.href}>
-      {props.link}
+    <nav style={props.style} className={props.className} >
+     <a href={props.route}>{props.linkName}</a>
     </nav>
   );
 }
--- src/components/NavigationComponents/NavLink.js
+++ src/components/NavigationComponents/NavLink.js
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
 
 /**
+ * Returns a nav element with a nested Link <a href="https://reactrouter.com/docs/en/v6/getting-started/installation">react-router-dom</a> element.
+ *
  * @param  {any} props
+ * @param {string} props.className - CSS Class name
+ * @param {object} props.style - Object representing a CSS Inline property class
+ * @param {string} props.route -  '/route-name' to provide Link.to attribute
+ * @param {string} props.linkName - name of the link ex. Home.
  */
-export default function NavLink(props){
-    return (
-      <nav style={props.style} className={props.className}>
-        <Link to={props.route}/>
-      </nav>
-    );
+export default function NavLink(props) {
+  return (
+    <nav style={props.style} className={props.className}>
+      <Link to={props.route}>{props.linkName}</Link>
+    </nav>
+  );
 }

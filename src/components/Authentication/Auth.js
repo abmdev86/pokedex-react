4e1711ed137846301aeecd972694ceaa195a67b0
Auroiah Morgan <amwebdev@outlook.com>
2022-07-13 09:12:11 -0400

    Update Auth.js

--- src/components/Authentication/Auth.js
+++ src/components/Authentication/Auth.js
@@ -1,11 +1,38 @@
 import Grid from "@mui/material/Grid";
+import CommonButton from "components/CommonComponents/CommonButton";
 
 import React from "react";
-
+const buttonStyles = {
+  fontSize: 20,
+  fontWeight: 700,
+  backgroundColor: "red",
+  "&:hover": {
+    backgroundColor: "yellow",
+  },
+};
 const Auth = () => {
+  const login = () => {
+    console.log("Logging in");
+  };
   return (
     <Grid item xs={8}>
       This is the Auth page
+      <CommonButton
+        sx={buttonStyles}
+        size="large"
+        variant="contained"
+        onClick={() => login()}
+      >
+        Login
+      </CommonButton>
+      <CommonButton
+        disable={true}
+        sx={buttonStyles}
+        size="large"
+        variant="contained"
+      >
+        Logout
+      </CommonButton>
     </Grid>
   );
 };

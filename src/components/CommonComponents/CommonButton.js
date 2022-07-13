4f0db12eb062efbf66c0a009ec51adcbc461783b
Auroiah Morgan <amwebdev@outlook.com>
2022-07-13 09:11:56 -0400

    Update CommonButton.js

--- src/components/CommonComponents/CommonButton.js
+++ src/components/CommonComponents/CommonButton.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Button from "@mui/material/Button";
-const CommonButton = ({ children, color, disable, variant, sx }) => {
+const CommonButton = ({ children, color, disable, variant, sx, onClick }) => {
   return (
-    <Button color={color} disabled={disable} variant={variant} sx={sx}>
+    <Button
+      color={color}
+      disabled={disable}
+      variant={variant}
+      sx={sx}
+      onClick={onClick}
+    >
       {children}
     </Button>
   );
